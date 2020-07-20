bb40625e3fd373e526ab392773e5b4c5cc37be5e
DQuangKhai <quangkhai27098@gmail.com>
2020-07-20 19:16:19 +0700

    vue.js

---
+++ vue js/new-tab-page/src/popup/router/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="popup">
+    <div class="popup-title">
+      <h4>Nhận hoàn tiền 10%</h4>
+      <p>Khi mua sắm tại đây</p>
+      <button class="btn btn-success">KÍCH HOẠT NGAY</button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+    .popup-title {
+        width: 300px;
+        padding: 10px;
+    }
+    
+    h4 {
+        font-size: 20px;
+        text-align: center;
+        color: red;
+    }
+    
+    p {
+        text-align: center;
+        display: block;
+    }
+    
+    .btn {
+        width: 100%;
+        text-align: center;
+        border-radius: 5px;
+    }
+</style>
---
+++ vue js/new-tab-page/src/tab/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <p>Loading...</p>
+    </div>
+    <div v-else>
+      <p class="joke">{{ joke }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      message: 'My new tab page',
+      joke: '',
+    };
+  },
+
+  mounted() {
+    axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } }).then(res => {
+      this.joke = res.data.joke;
+      this.loading = false;
+    });
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+body {
+  height: 98vh;
+  text-align: center;
+  color: #353638;
+  font-size: 22px;
+  line-height: 30px;
+  font-family: Merriweather, Georgia, serif;
+  background-size: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.joke {
+  max-width: 800px;
+}
+</style>
